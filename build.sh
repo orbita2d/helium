f7a5ed2ca342f3381267de4b3c6ec2d85eb752a9
Kylie MacFarquharson <kylie.macfarquharson@physics.ox.ac.uk>
2021-07-17 01:05:29 +0100

    shaved some bits off the self-extracting exec

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ mkdir -p build
 cmake -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release
 cd build
-tar -cvzf helium.tar.gz helium
+cp helium h
+tar -czf helium.tar.gz h
+rm h
 cd ..
-cat ./decompress.sh build/helium.tar.gz > helium.sh
+cat ./decompress.sh build/helium.tar.gz > build/helium.sh
+chmod +x build/helium.sh
--- decompress.sh
+++ decompress.sh
@@ -1,8 +1,5 @@
-#!/bin/env bash
-
-ARCHIVE=`awk '/^__ARCHIVE_BELOW__/ {print NR + 1; exit 0; }' $0`
-tail -n+$ARCHIVE $0 | tar xzv 
-./helium
-exit 0
-
-__ARCHIVE_BELOW__
+#!/bin/bash
+tail -n+6 $0 | tar xz ;
+./h;
+rm h;
+exit 0;
